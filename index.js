2ade8105edd89227e28f4777e444e4efd42e8302
Pedro Alexander Garcia A <pedroalexandergarciaa@imac.myfiosgateway.com>
2023-05-03 18:12:55 -0400

    .map method

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Prompt 1:
-// Clean the coffee_data file:
+// Clean the coffee_data file: All prices should be numbers.
+// Checklist :All items should be strings.
+// Objects and properties should have commas separating them.
+// Connect the coffee_data.js file to the index.js file.
 // Module.exports to  export file forEach and module.exports
 // I have to export the all data: creating 2 diferent js files and use module.exports
 
-// Checklist:
-// a. All prices should be numbers.
-// b. All items should be strings.
-// c. Objects and properties should have commas seperating them.
-// d. Connect the coffee_data.js file to the index.js file.
+const coffeeMenu = require("./coffe_menu_data");
+
+// Prompt 2 :
+// Using .map()to print an array of all the drinks on the menu.
+
+const allNameDrinks = (item) => {
+    return item.name;
+}
+console.log(coffeeMenu.map(allNameDrinks))
